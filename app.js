752579dc0eb99555744432b2340828f28131f96b
unknown <carvalhoanthony14@gmail.com>
2025-11-05 13:34:12 +0530

    updates

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
+import { bubbleSort } from '/algorithms/bubbleSort.js';
+import { selectionSort } from '/algorithms/selectionSort.js';
+import { insertionSort } from '/algorithms/insertionSort.js';
+import { mergeSort } from '/algorithms/mergeSort.js';
+import { quickSort } from '/algorithms/quickSort.js';
+import { renderArray, swap, delay } from './utils/helpers.js';
+
 let array = [];
-let speed = 50;
+let arraySize = 50;
+let speed = 250;
+let algorithm = 'bubbleSort';
+const arrayContainer = document.getElementById('array-container');
 
-document.getElementById('generate').addEventListener('click', generateArray);
-document.getElementById('start').addEventListener('click', startSorting);
-document.getElementById('speed').addEventListener('input', e => speed = e.target.value);
-
-function generateArray(size = 30) {
-  array = Array.from({ length: size }, () => Math.floor(Math.random() * 100) + 5);
-  renderArray(array);
+function generateArray() {
+    array = [];
+    for (let i = 0; i < arraySize; i++) {
+        array.push(Math.floor(Math.random() * 400) + 10);
+    }
+    renderArray(array, arrayContainer);
 }
 
-async function startSorting() {
-  const algorithm = document.getElementById('algorithm').value;
-  if (algorithm === 'bubbleSort') await bubbleSort(array);
-  renderArray(array);
-}
+document.getElementById('array-size').addEventListener('input', (e) => {
+    arraySize = e.target.value;
+    generateArray();
+});
+
+document.getElementById('speed').addEventListener('input', (e) => {
+    speed = e.target.value;
+});
+
+document.getElementById('algorithm-select').addEventListener('change', (e) => {
+    algorithm = e.target.value;
+});
+
+document.getElementById('new-array').addEventListener('click', generateArray);
+
+document.getElementById('start').addEventListener('click', async () => {
+    switch(algorithm) {
+        case 'bubbleSort':
+            await bubbleSort(array, arrayContainer, speed);
+            break;
+        case 'selectionSort':
+            await selectionSort(array, arrayContainer, speed);
+            break;
+        case 'insertionSort':
+            await insertionSort(array, arrayContainer, speed);
+            break;
+        case 'mergeSort':
+            await mergeSort(array, arrayContainer, speed);
+            break;
+        case 'quickSort':
+            await quickSort(array, arrayContainer, speed);
+            break;
+    }
+});
 
 generateArray();
